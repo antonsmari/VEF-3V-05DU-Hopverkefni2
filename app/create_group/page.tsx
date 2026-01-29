b10e27c7826149451bd3788cf1a7262822e1cb67
Ingibjörg Ösp Finnsdóttir <osp07@users.noreply.github.com>
2026-01-29 22:25:21 +0000

    Create Event HTML page set up

---
+++ app/create_group/page.tsx
@@ -0,0 +1,17 @@
+import Form from "next/form";
+export default function CreateGroup() {
+  return(
+    <div>
+      <Form action="">
+        <label>Title the group event</label>
+        <input type="text" required/>
+        <label>Description (optional)</label>
+        <textarea/>
+        <label>Start date</label>
+        <input type="date" required/>
+        <label>End date (optional)</label>
+        <button type="submit">Submit</button>
+      </Form>
+    </div>
+  )
+}
