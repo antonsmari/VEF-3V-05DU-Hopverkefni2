ffeb151ed5b39c1da805074e020f9ecdf89f721e
Anton Smári Gunnarsson <anton@fail.is>
2026-01-29 20:31:54 +0000

    Login inputs fixed and labels/placeholder added

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,29 @@
-import Form from 'next/form';
-export default function Login(){
-  return (
-      <div>
-          <Form action="">
-            <input type='text'>Name:</input>
-            <input type='email'>Email:</input>
-            <input type='password'>Password:</input>
-            <button type="submit">Submit</button>
-          </Form>
-      </div>
-    )
+import Form from "next/form";
+export default function Login() {
+	return (
+		<div>
+			<Form action="">
+				<label htmlFor="name">Name:</label>
+				<input id="name" type="text" name="name" placeholder="Name" />
+
+				<label htmlFor="email">Email:</label>
+				<input
+					id="email"
+					type="email"
+					name="email"
+					placeholder="Email"
+				/>
+
+				<label htmlFor="password">Password:</label>
+				<input
+					id="password"
+					type="password"
+					name="password"
+					placeholder="Password"
+				/>
+
+				<button type="submit">Submit</button>
+			</Form>
+		</div>
+	);
 }
