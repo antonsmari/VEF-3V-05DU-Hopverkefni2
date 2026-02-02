068ef11fa57a80b479f06de018cbe4413baacf45
Numi Hrafn Baldursson <numihrafnbaldursson@gmail.com>
2026-02-02 14:38:41 +0000

    added functionality to register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,8 +1,19 @@
 import Form from "next/form";
+import { createUser} from "@/db/repo/usersRepo";
+import bcrypt from "bcryptjs";
 export default function Register() {
+	async function newUser(formData:FormData) {
+		"use server";
+		createUser({
+			displayName: formData.get("name") as string,
+			email: formData.get("email") as string,
+			passwordHash: await bcrypt.hash(formData.get("password") as string, 10),
+		});
+
+	}
 	return (
 		<div>
-			<Form action="">
+			<Form action={newUser}>
             /* put register information in a session of logged in users*/
             /* reroutes to an enviroment for a logged in user*/
                 <label htmlFor="name">Name:</label>
