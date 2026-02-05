f8e43b052088f78ab5d3c63ca094d5a91556a8fc
Anton Smári Gunnarsson <anton@fail.is>
2026-02-05 19:12:18 +0000

    Added a group page that shows overview of members and transactions

---
+++ app/group/[id]/page.tsx
@@ -0,0 +1,53 @@
+import { getGroupById, listGroupMembers } from "@/db/repo/groupsRepo";
+import { listTransactionsForGroup } from "@/db/repo/transactionsRepo";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+export default async function Group({
+	params,
+}: {
+	params: Promise<{ id: string }>;
+}) {
+	const { id } = await params;
+	if (isNaN(Number(id))) {
+		redirect("/user/dashboard");
+	}
+
+	const group = await getGroupById(Number(id));
+	const groupMembers = await listGroupMembers(Number(id));
+	const groupTransactions = await listTransactionsForGroup(Number(id));
+
+	if (!group || !groupMembers) {
+		redirect("/user/dashboard");
+	}
+
+	return (
+		<div>
+			<h1>{group.name}</h1>
+
+			<Link href={`/group/${group.id}/transaction/new`}>
+				Add New Transaction
+			</Link>
+
+			<h2>Members:</h2>
+			<ul>
+				{groupMembers.map((member) => (
+					<li key={member.users.id}>
+						{member.users.displayName} ({member.users.email}) -{" "}
+						{member.group_members.role}
+					</li>
+				))}
+			</ul>
+
+			<h2>Transactions:</h2>
+			<ul>
+				{groupTransactions.map((transaction) => (
+					<li key={transaction.id}>
+						{transaction.title} - {transaction.totalAmount}{" "}
+						(Occurred at: {transaction.occurredAt.toDateString()})
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+}
