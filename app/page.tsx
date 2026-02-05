f675c117ee3af04cfbc2dc11d90b03d2e66f7b28
Anton Smári Gunnarsson <anton@fail.is>
2026-02-05 17:05:58 +0000

    Modified our front page to include English description because why not?

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,26 @@ export default function Home() {
 	return (
 		<div>
 			<p>
-				vefsíðan okkar hentar þér því að hún getur haldið utan um
-				skuldir og eignir hópsins á einfaldan hátt. Með því að nota
+				Íslenska: Vefsíðan okkar hentar þér því að hún getur haldið utan
+				um skuldir og eignir hópsins á einfaldan hátt. Með því að nota
 				síðuna okkar getur þú og þinn hópur forðast misskilning og
 				ágreining um peninga og einblínt á skemmtilegar stundir saman!
 			</p>
+			<p>
+				English: Our website is suitable for you because it can keep
+				track of the debts and assets of the group in a simple way. By
+				using our website, you and your group can avoid
+				misunderstandings and disagreements about money and focus on
+				having fun together!
+			</p>
 			<div>
-				<p>log in</p>
+				<p>Login</p>
 				<button>
 					<a href="/login">Login</a>
 				</button>
 			</div>
 			<div>
-				<p>sign up</p>
+				<p>Sign Up</p>
 				<button>
 					<a href="/register">Sign Up</a>
 				</button>
