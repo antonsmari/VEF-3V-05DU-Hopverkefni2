a42a891fd20c4db4a94f1060ba1fa7a7512daabd
Anton Smári Gunnarsson <anton@fail.is>
2026-02-05 16:45:12 +0000

    requireUser helper function added that also fetches the user from database and returns it

--- lib/auth/requireUser.ts
+++ lib/auth/requireUser.ts
@@ -1,9 +1,17 @@
 import "server-only";
 import { redirect } from "next/navigation";
 import { getDbSession } from "@/lib/auth/session";
+import { getUserById } from "@/db/repo/usersRepo";
 
 export async function requireUserId() {
 	const session = await getDbSession();
 	if (!session) redirect("/login");
 	return session.userId;
+}
+
+export async function requireAndGetUser() {
+	const id = await requireUserId();
+	const user = await getUserById(id);
+	if (!user) redirect("/login");
+	return user;
 }
